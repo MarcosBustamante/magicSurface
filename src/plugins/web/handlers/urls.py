66a32a3117fbbb8e5e9cd9f369c8cc9b1e84b5a9
iury <satriani-16@hotmail.com>
2015-07-19 20:49:48 -0300

    abacate

--- src/plugins/web/handlers/urls.py
+++ src/plugins/web/handlers/urls.py
@@ -1,13 +1,31 @@
 # coding utf-8
 import webapp2
+from src.core.web.mshandler import MSHandler
 from src.plugins.web.handlers.api.layer import layer
 from src.plugins.web.handlers.api.file import file
 from src.plugins.web.handlers.doc import doc
 from src.plugins.web.handlers.apps import myApps
 from src.plugins.web.handlers.sign import signUp, signIn, signOut
-
+import vimeo
+import youtube
+from requests.packages import urllib3
+urllib3.disable_warnings()
+from google.appengine.api import urlfetch
+urlfetch.set_default_fetch_deadline(60)
 __author__ = 'bustamante'
 
+class Teste(MSHandler):
+    def get(self):
+        self.write_template('teste.html')
+
+    def post(self):
+        file_item = self.request.POST.get('file')
+        v = vimeo.VimeoClient(
+            token="89c113ec6b8847ba634737d3b8e28071",
+            key="f0d0349846dce902a20bde2cef98170e76ea92f6",
+            secret="DzmUvtyDRHvh1hfo5lhn9Qm9VNZ8h0+Gw9V3hHKSLZQmrXWu00SqCqfsZQ4dJlcJpWtRjE2N+1wPhxa8dP6dI3b0qX/1JqJJIt0i0NRIsqVi+Ivr2+ReGmQlshASrUud")
+
+        v.upload(file_item)
 
 site_routes = [
     ('/', doc.DocHandler),
@@ -15,7 +33,8 @@ site_routes = [
     ('/signOut', signOut.SignOutHandler),
     ('/signIn', signIn.SignInHandler),
     ('/myApps', myApps.MyAppsHandler),
-    ('/documentation', doc.DocHandler)
+    ('/documentation', doc.DocHandler),
+    ('/teste', Teste)
 ]
 api_routes = [
     ('/layer/get', layer.LayerGetHandler),
---
+++ youtube.py
@@ -0,0 +1,117 @@
+import httplib
+import httplib2
+import os
+import random
+import sys
+import time
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+
+
+# Explicitly tell the underlying HTTP transport library not to retry, since
+# we are handling retry logic ourselves.
+httplib2.RETRIES = 1
+
+# Maximum number of times to retry before giving up.
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
+  httplib.IncompleteRead, httplib.ImproperConnectionState,
+  httplib.CannotSendRequest, httplib.CannotSendHeader,
+  httplib.ResponseNotReady, httplib.BadStatusLine)
+
+# Always retry when an apiclient.errors.HttpError with one of these status
+# codes is raised.
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# This OAuth 2.0 access scope allows an application to upload files to the
+# authenticated user's YouTube channel, but doesn't allow other types of access.
+YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+# This variable defines a message to display if the CLIENT_SECRETS_FILE is
+# missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the Developers Console
+https://console.developers.google.com/
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
+
+
+def get_authenticated_service(args):
+  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+    scope=YOUTUBE_UPLOAD_SCOPE,
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+  storage = Storage("%s-oauth2.json" % __name__)
+  credentials = storage.get()
+
+  if credentials is None or credentials.invalid:
+    print args
+    credentials = run_flow(flow, storage, args)
+
+  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    http=credentials.authorize(httplib2.Http()))
+
+def initialize_upload(youtube, options):
+  tags = None
+  if options.keywords:
+    tags = options.keywords.split(",")
+
+  body=dict(
+    snippet=dict(
+      title=options.title,
+      description=options.description,
+      tags=tags,
+      categoryId=options.category
+    ),
+    status=dict(
+      privacyStatus=options.privacyStatus
+    )
+  )
+
+  # Call the API's videos.insert method to create and upload the video.
+  insert_request = youtube.videos().insert(
+    part=",".join(body.keys()),
+    body=body,
+    # The chunksize parameter specifies the size of each chunk of data, in
+    # bytes, that will be uploaded at a time. Set a higher value for
+    # reliable connections as fewer chunks lead to faster uploads. Set a lower
+    # value for better recovery on less reliable connections.
+    #
+    # Setting "chunksize" equal to -1 in the code below means that the entire
+    # file will be uploaded in a single HTTP request. (If the upload fails,
+    # it will still be retried where it left off.) This is usually a best
+    # practice, but if you're using Python older than 2.6 or if you're
+    # running on App Engine, you should set the chunksize to something like
+    # 1024 * 1024 (1 megabyte).
+    media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
+  )
+
+def upload(options):
+  youtube = get_authenticated_service(options)
+  try:
+    initialize_upload(youtube, options)
+  except HttpError, e:
+    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
